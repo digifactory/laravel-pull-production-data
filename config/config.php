80963d3de35acc5f7e525c582bc90ee17ab32be2
Maurice <maurice@digifactory.nl>
2019-11-29 17:55:24 +0100

    Update config.php
    it is larger database "or" storage size

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ return [
     'deploy_server' => env('DEPLOY_SERVER'),
     'deploy_path' => env('DEPLOY_PATH'),
 
-    'timeout' => 900, // Set default to 15 minutes for larger database of storage size
+    'timeout' => 900, // Set default to 15 minutes for larger database or storage size
 
     'database_connection' => config('database.default'),
 
