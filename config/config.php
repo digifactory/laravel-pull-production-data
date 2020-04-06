488111966e2229453418033807dd8facae0b5712
Mark <mrk-j@users.noreply.github.com>
2020-04-06 13:55:45 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -11,6 +11,6 @@ return [
     'paths' => [
         'mysql' => 'mysql', // Path to mysql binary on your local machine
         'mysqldump' => 'mysqldump', // Path to mysqldump binary on remote server
-        'env-prefix-path' => 'public_html/', // The path from where the ssh client enters to the .env file
+        'env-prefix-path' => 'public_html/', // Path to the .env file relative to the SSH user's home directory
     ],
 ];
